a432e7072d40d3ed0231f03160c09f57adb7d142
rana shehibar <ranashehibar@gmail.com>
2024-11-14 14:09:09 +0200

    my first commit

---
+++ example.js
@@ -0,0 +1,20 @@
+let myName = "rana";
+myName = "nndfgd";
+console.log(myName);
+
+console.log(student.name)
+let students = ['john','jane','jack']
+
+console.log(student[0]), student[1] , student[2]
+
+person.age
+function average(){
+    let result = (a+b)/2
+    return result
+}
+
+
+average(4,2)
+average(10,2)
+console.log(average(4,2))
+console.log(average(10,2))
